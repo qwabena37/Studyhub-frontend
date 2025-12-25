9d9199db124baab3dc213f19732c2201b2bae6f7
James <qjaymce37@gmail.com>
2025-12-25 17:54:03 +0000

    modify src/pages/CreateProject.jsx

---
+++ src/pages/CreateProject.jsx
@@ -0,0 +1,9 @@
+const CreateProject = () => {
+  return (
+    <div className="p-6">
+      <h1 className="text-xl font-bold">Create Project</h1>
+    </div>
+  );
+};
+
+export default CreateProject;
